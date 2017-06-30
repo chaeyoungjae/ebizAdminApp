a6e6e4344d81f06383515ae3c62c2d5f535f200a
chaester <yubs1004@gmail.com>
2017-06-30 18:00:01 +0900

    typescript

--- src/app/comp/direc/date-pick.directive.ts
+++ src/app/comp/direc/date-pick.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
-
+import {Directive, ElementRef, Input} from '@angular/core';
+declare var jQuery : any;
 @Directive({
   selector: '[appDatePick]'
 })
 export class DatePickDirective {
-
-  constructor() { }
+  @Input() options : Object = {};
+  constructor(private elementRef : ElementRef) {
+    jQuery(this.elementRef).date
+  }
 
 }
